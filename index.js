0c25746a25e76cd4ffcbc4b945967a9ab29c7265
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-13 00:05:17 -0500

    better ext matching

--- index.js
+++ index.js
@@ -5,19 +5,6 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
-/**
- * Match groups:
- *
- *   0 => full path
- *   1 => dirname
- *   2 => basename (name with ext)
- *   3 => name (without ext)
- *   4 => extname
- *   5 => ext (without .)
- */
-
 module.exports = function pathRegex() {
-  return /([\s\S]*?)(([^\\\/.]*?)(\.([^.\\\/]*)$|$))/;
+  return /([\s\S]*?)(([^\\\/.]*?)((\.([^\\\/.]*))*)|$)$/;
 };
